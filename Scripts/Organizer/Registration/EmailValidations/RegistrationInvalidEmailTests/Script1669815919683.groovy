458d2999295f6e86d59dd90643b6c5deff079dc1
harshitha <surendrareddyharshitha012@gmail.com>
2022-12-10 17:08:36 +0000

    working Code merged

--- Scripts/Organizer/Registration/EmailValidations/RegistrationInvalidEmailTests/Script1669815919683.groovy
+++ Scripts/Organizer/Registration/EmailValidations/RegistrationInvalidEmailTests/Script1669815919683.groovy
@@ -23,17 +23,13 @@ WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
 
-
 // ------------------------------Valid email tests-------------------------------------
-
 //--1) email id exceeding 320 chars -> invalid
-
 WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Email Address_email'), 'hhhhhhnhjhjhjhjhjhjhsjdbjhsgdfjegrfjhddfjhgyersgjdshcbdhsvdhsfghjgfjhsgdhjsdfnhsgfdjhgdfhdsvjhfdhgdfjhsdgjdfgyjhgsjdmbvhsjgvyfdhjgbvndhjgfgvefhjvdbcjhdvghjefgeyrughdbfcnvfgehdsbvcsvhsjfgdjhfvhjfgdnhvbcbnnnnbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjhhjghghghjhkgjhhhhnhvg@gmail.com')
 
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 //--2) email with missing @ -> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
@@ -43,7 +39,6 @@ WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi da
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 //--3) email without domain -> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
@@ -53,7 +48,6 @@ WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi da
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 //--4) email starting with a dot -> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
@@ -63,7 +57,6 @@ WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi da
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 //--5) email with two @s -> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
@@ -73,7 +66,6 @@ WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi da
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 //-6) email with two dots -> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
@@ -83,52 +75,48 @@ WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi da
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 //-7) email with different language -> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
 
 WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Email Address_email'), 'おえあいう@example.com')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Create Password_password'),
-	'cvW8qx4B2o3F4VwP/kNsqA==')
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Create Password_password'), 
+    'cvW8qx4B2o3F4VwP/kNsqA==')
 
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/button_Next'))
 
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Please enter a valid email'))
 
 //-8) email name with extra chars after .com -> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
 
 WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Email Address_email'), 'a@gmail.comaa')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Create Password_password'),
-	'cvW8qx4B2o3F4VwP/kNsqA==')
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Create Password_password'), 
+    'cvW8qx4B2o3F4VwP/kNsqA==')
 
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/button_Next'))
 
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Please enter a valid email'))
 
 //-9) email name hiphen before addres-> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
 
 WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Email Address_email'), 'a@-gmail.com')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Create Password_password'),
-	'cvW8qx4B2o3F4VwP/kNsqA==')
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/input_Create Password_password'), 
+    'cvW8qx4B2o3F4VwP/kNsqA==')
 
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/button_Next'))
 
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Please enter a valid email'))
 
 //-10) email name two dots after @-> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
@@ -138,7 +126,6 @@ WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi da
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 //-11) email name number after @-> invalid
-
 WebUI.navigateToUrl('https://organizer-testing.formi.co.in/')
 
 WebUI.click(findTestObject('Object Repository/Page_Weve got your back/a_Register Yourself'))
@@ -148,6 +135,4 @@ WebUI.setText(findTestObject('Object Repository/Page_Sign-up to get the Formi da
 WebUI.click(findTestObject('Object Repository/Page_Sign-up to get the Formi dashboard/div_Invalid email format'))
 
 WebUI.closeBrowser()
-
-
 
--- Test Suites/OTP.ts
+++ Test Suites/OTP.ts
@@ -15,28 +15,28 @@
       <guid>e7341eea-1b7c-4033-9b50-88b8164ba859</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/OTP - REFER/Valid OTP - Expiry</testCaseId>
+      <testCaseId>Test Cases/Organizer/OTP/Valid OTP - Expiry</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>43c2f3b8-5b31-4a46-83db-bcc68c364bd2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/OTP - REFER/Valid OTP - Resend</testCaseId>
+      <testCaseId>Test Cases/Organizer/OTP/Valid OTP - Resend</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>0a838879-03d0-4c38-b68c-63f3bb84a995</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/OTP - REFER/Valid OTP - Wrong Number</testCaseId>
+      <testCaseId>Test Cases/Organizer/OTP/Valid OTP - Wrong Number</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>150015fb-518e-4c45-9f68-5729ba64be10</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/OTP - REFER/Valid OTP</testCaseId>
+      <testCaseId>Test Cases/Organizer/OTP/Valid OTP</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
