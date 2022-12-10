6c019776ce33b45b71bcbcebe6fac55f39817920
harshitha <surendrareddyharshitha012@gmail.com>
2022-12-10 17:16:43 +0000

    File name rename

--- Test Suites/Google Sign up.ts
+++ Test Suites/Google Sign up.ts
@@ -15,70 +15,70 @@
       <guid>12a72cbb-c6e4-4550-95fe-a988e87dcedf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/Create Account</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/Create Account</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>651682d9-d31e-4a88-9d27-6adb7b8ec925</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/First Name</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/First Name</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b16069bc-a8b7-4d95-ab45-2ac18df19513</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/Last Name</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/Last Name</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>21f28436-2f03-47f5-b48a-1eca90148338</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/User name</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/User name</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>69bdf66f-329c-454f-9152-537eee9b0fde</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/Password</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/Password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>0a7f3b52-7d08-41b3-bf3e-37e94afbcec0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/Confirm Password</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/Confirm Password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>164237cf-d4b8-4c49-9bed-ade6d3c42cba</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/View Password</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/View Password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>bd4e7375-0bd5-4b45-ab10-41128a3dfbd2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/Next Button</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/Next Button</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>017034fe-fecc-4993-9200-0e5a47ae02dd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/Phone Number</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/Phone Number</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>d62280f6-e702-4a90-9e08-2ba7ed8775d1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Organizer/GSU - REFER/Sign In Instead</testCaseId>
+      <testCaseId>Test Cases/Organizer/GSU/Sign In Instead</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
